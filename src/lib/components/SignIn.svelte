4a4af22ba70525c2e7cb6ca944a244166a5c7d63
jameseidson <eidsonje@icloud.com>
2022-12-18 23:18:24 -0700

    added sign in page

--- src/lib/components/SignIn.svelte
+++ src/lib/components/SignIn.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
   import { googleSignIn } from "$lib/utilities/firebase";
+  import logo from "$lib/assets/logo-purple-transparent.png";
 </script>
 
-<button on:click={googleSignIn} class="btn btn-primary">Sign In</button>
+<div class="grid place-items-center h-screen">
+  <div
+    class="card place-items-center p-4 space-y-4 w-100 bg-neutral shadow-xl "
+  >
+    <img class="max-w-sm" src={logo} alt="RCCNU logo" />
+    <div
+      class="tooltip tooltip-info w-full"
+      data-tip="Please sign in with your NU email!"
+    >
+      <button on:click={googleSignIn} class="btn btn-primary btn-block"
+        >Sign In</button
+      >
+    </div>
+  </div>
+</div>
