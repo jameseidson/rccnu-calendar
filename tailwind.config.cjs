ec4d0511bd6be5cf632f3db3865e0b0c539e2633
jameseidson <eidsonje@icloud.com>
2022-12-18 23:00:22 -0700

    configured daisyui theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        default: {
+          primary: "#4f2e82",
+          secondary: "#9B2A7D",
+          accent: "#1FB2A6",
+          neutral: "#191D24",
+          "base-100": "#2A303C",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
   },
 
   plugins: [require("daisyui")],
