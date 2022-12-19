459a11562e7878528fcb7428556b9c02ef090b0f
jameseidson <eidsonje@icloud.com>
2022-12-18 19:46:00 -0700

    installed and initialized firebase

---
+++ src/lib/utilities/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY6A_i48q4BghB7PQNjCJneEzlzfAeozQ",
+  authDomain: "rccnu-calendar.firebaseapp.com",
+  databaseURL: "https://rccnu-calendar-default-rtdb.firebaseio.com",
+  projectId: "rccnu-calendar",
+  storageBucket: "rccnu-calendar.appspot.com",
+  messagingSenderId: "616570603818",
+  appId: "1:616570603818:web:d50540c4e2c28c58b2f653",
+};
+
+const app = initializeApp(firebaseConfig);
